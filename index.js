d1d1758dfd3c0d7c08b83ebf47403bd0ca1891ae
Thiago <thiago.andrade.devs@gmail.com>
2021-12-13 17:04:42 -0300

    servidor

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
+const app = express();
+const port = 3001;
+app.listen(port, () => console.log(`Servidor inicializado na porta ${port}`));
