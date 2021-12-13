d295dcd97f4cf8a8a41c17acfe89b6ab29b63db2
Thiago <thiago.andrade.devs@gmail.com>
2021-12-13 18:03:34 -0300

    servidor

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { application, response } = require('express');
 
 
 const app = express();
 const port = 3001;
+app.use(cors());
+app.use(bodyParser.json());
+
+// get, post, put, delete
+
+
+//listar todas as tarefa  - get
+app.get(' /gerenciador-tarefas', (req, res) => {
+  res.status(501).json({ erro: `erro nao implementado` })
+})
+//listar uma tarefa por id -get
+//cadastrar uma tarefa  -post
+//atualizar uma tarefa- put
+//remover uma tarefa -delete
+//concluir uma tarefa -put
+
+
 app.listen(port, () => console.log(`Servidor inicializado na porta ${port}`));
