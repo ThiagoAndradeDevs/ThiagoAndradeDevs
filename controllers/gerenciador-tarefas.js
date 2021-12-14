81073d4a38e68838c3ee6c6b8d9cd88eb2f1302e
Thiago <thiago.andrade.devs@gmail.com>
2021-12-14 14:41:27 -0300

    uuidv4

---
+++ controllers/gerenciador-tarefas.js
@@ -0,0 +1,9 @@
+const uuidv4 = require('uuid/4');
+
+let tarefas = [
+  { id: '1', nome: 'Aprender React', concluida: true },
+  { id: '2', nome: 'Aprender Padroes de Projetos', concluida: false },
+  { id: '3', nome: 'Aprender Javascript', concluida: false },
+  { id: '4', nome: 'Aprender React usando Hooks', concluida: false },
+];
+
