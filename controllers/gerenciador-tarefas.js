1af5dcbc862ce1a6a7b47295fb413777fc4feae2
Thiago <thiago.andrade.devs@gmail.com>
2021-12-14 15:16:33 -0300

    listar

--- controllers/gerenciador-tarefas.js
+++ controllers/gerenciador-tarefas.js
@@ -16,7 +16,28 @@ function listarTarefasId(req, res) {
   res.status(tarefa[0]);
 }
 function listarTarefa(req, res) {
-
+  const pagina = req.query['pag'] || 1;
+  const ordem = req.query['ordem']; // ASC , DESC
+  const filtroTarefa = req.query['filtro-tarefa'];
+  const itensPorPagina = req.query['itensPorPagina'] || 3;
+  let tarefasRetornar = tarefas.slice(0);
+  // filtro
+  if (tarefasRetornar) {
+    tarefasRetornar = tarefasRetornar.filter(
+      t => t.nome.toLowerCase().indexOf(filtroTarefa.toLowerCase()) === 0);
+  }
+  //ordenar
+  if (ordem === 'ASC') {
+    tarefasRetornar.sort((t1, t2) => (t1.nome.toLowerCase() > t2.nome.toLowerCase()) ? 1 : -1);
+  } else if (ordem === 'DESC') {
+    tarefasRetornar.sort((t1, t2) => (t1.nome.toLowerCase() < t2.nome.toLowerCase()) ? 1 : -1);
+  }
+  //retornar
+  res.json({
+    totalItens: tarefasRetornar.length,
+    tarefas: tarefasRetornar.slice(0).splice((pagina - 1) * itensPorPagina, itensPorPagina),
+    pagina: pagina
+  })
 }
 module.exports = {
   listarTarefasId,
